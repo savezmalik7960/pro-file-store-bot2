b453f0886f0963942725706fc31384a49baa15a3
savezmalik7960 <101207459+savezmalik7960@users.noreply.github.com>
2022-04-09 07:17:38 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -63,7 +63,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/malik_support_group).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
